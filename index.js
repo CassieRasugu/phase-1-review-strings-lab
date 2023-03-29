ffa153f62d6d5c761179bf067b61ba387d69f22d
Cassie <cassierasugu@gmail.com>
2023-03-29 10:27:20 +0300

    Initialize Commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code in this file!
+const currentUser = "Cassie Rasugu";
+const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
+
+const myString = "template literal";
+const myNumber = 10;
+const myBoolean = false;
+
+const exicitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting ='Welcome, ${currentUser.slice(0,1)}!';
+
+
+
+
+
+
+
+
+
